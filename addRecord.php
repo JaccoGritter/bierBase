47e29940c02ea40564a6a25656f76ac7ac96a1f7
Jacco Gritter <jacco.gritter@gmail.com>
2019-05-28 14:49:04 +0200

    Testrecord toevoegen lukt. Eigen invoer (nog) niet

--- addRecord.php
+++ addRecord.php
@@ -2,13 +2,29 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "bierbase";
+
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+
+//echo "Connected successfully";
+//$_POST["email"]
+
+$sql = "INSERT INTO bieren (brouwerij, naam, land, type, alcoholpercentage, score, opmerkingen) VALUES ('Hertog Jan', 'Triple', 'NL', 'Triple', 9.1, 8.0, 'Heerlijk biertje!')";
+
+// $sql = "INSERT INTO bieren (brouwerij, naam, land, biertype, alcoholpercentage, score, opmerkingen) VALUES ($_POST['brouwerij'], $_POST['naam'], $_POST['land'], $_POST['type'], $_POST['alcoholpercentage'], $_POST['score'], $_POST['opmerkingen')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
--- biertoevoegen.php
+++ biertoevoegen.php
@@ -7,20 +7,24 @@
     <title>bierBase record toevoegen</title>
 </head>
 <body>
-    <form action = "addRecord.php">
+    <form action = "addRecord.php" method = "POST">
     Bouwerij:<br>
     <input type="text" name="brouwerij"><br>
     Naam van het bier<br>
-    <input type="text" name="biernaam"><br>
+    <input type="text" name="naam"><br>
     Land van herkomst<br>
-    <input type = "text" name = "lvh" maxlength="2"><br>
+    <input type = "text" name = "land" maxlength="2"><br>
+    Type van het bier<br>
+    <input type="text" name="type"><br>
     Alcoholpercentage<br>
-    <input type = "number" name = "alcperc" min = "0" max = "15" step = "0.1"><br>
+    <input type = "number" name = "alcoholpercentage" min = "0" max = "15" step = "0.1"><br>
     Score (0-10)<br>
     <input type = "number" name = "score" min = "0" max = "10" step = "0.5"><br>
     Opmerkingen <br>
-    <textarea name="message" style="width:20rem; height:5rem;"></textarea><br>
-    <input type="submit" value = "verzend">
+    <textarea name="opmerkingen" style="width:20rem; height:5rem;"></textarea><br>
+    <input type="submit" value = "voeg toe">
     </form>
+    <br>
+    <a href="index.php">Home</a>
 </body>
 </html>
