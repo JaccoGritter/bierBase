32ed1e5492fadf3000e2159171a33cb9551a4e3f
Jacco Gritter <jacco.gritter@gmail.com>
2019-05-29 12:21:37 +0200

    toonbieren added

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>bierBase</title>
 </head>
+
 <body>
     <h2>Maak een keuze:</h2>
-    <a href="biertoevoegen.php">Voeg een bier toe</a>
+    <a href="biertoevoegen.php">Voeg een bier toe</a><br>
+    <a href="toonbieren.php">Laat de bieren zien</a>
 </body>
+
 </html>
